8201fe27898f70b9eba24defd39cc7ae7c029806
Luis Felipe <luisfelipesdn12@gmail.com>
2022-01-08 01:17:48 -0300

    feat: Add new "I am Rich" app
    From section 3 of the course https://www.udemy.com/course/flutter-bootcamp-with-dart/learn/lecture/14482060#overview

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/redical-diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
