2ac3058eaaa6251f212ce5d13d34e7604482e323
Luis Felipe <luisfelipesdn12@gmail.com>
2022-01-10 19:44:27 -0300

    feat: Initial mi card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,36 +9,118 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple[100],
+        backgroundColor: Colors.indigo[50],
         body: SafeArea(
           child: Center(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.purple[200],
-                  width: 100,
+                CircleAvatar(
+                  radius: 90.0,
+                  backgroundColor: Colors.teal[200],
+                  backgroundImage:
+                      NetworkImage("http://github.com/luisfelipesdn12.png"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Luis Felipe",
+                  style: TextStyle(
+                    fontFamily: "Asap",
+                    fontSize: 40.0,
+                    color: Colors.grey[850],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "Software Developer",
+                  style: TextStyle(
+                    fontFamily: "Asap",
+                    fontSize: 25.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
                 ),
                 Container(
-                  width: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        color: Colors.purple[200],
-                        height: 100,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[400],
+                        offset: Offset(0.0, 1.0),
+                        blurRadius: 3.0,
                       ),
-                      Container(
-                        color: Colors.purple[300],
-                        height: 100,
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.grey[850],
+                        size: 25.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "luisfelipesdn12@gmail.com",
+                        style: TextStyle(
+                          fontFamily: "Asap",
+                          fontSize: 20.0,
+                          color: Colors.grey[600],
+                        ),
                       ),
                     ],
                   ),
                 ),
+                SizedBox(
+                  height: 10.0,
+                ),
                 Container(
-                  color: Colors.purple[300],
-                  width: 100,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[400],
+                        offset: Offset(0.0, 1.0),
+                        blurRadius: 3.0,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.link,
+                        color: Colors.grey[850],
+                        size: 25.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "luisfelipesdn12.now.sh",
+                        style: TextStyle(
+                          fontFamily: "Asap",
+                          fontSize: 20.0,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
