e1d8f9e69befa763bd421db5979dde9e196bab00
Luis Felipe <luisfelipesdn12@gmail.com>
2022-01-18 20:47:20 -0300

    feat: 🍱 Build initial layout

--- bmi-calculator-flutter/lib/screens/input.screen.dart
+++ bmi-calculator-flutter/lib/screens/input.screen.dart
@@ -13,12 +13,55 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  PageContainer(),
+                  PageContainer(),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  PageContainer(),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  PageContainer(),
+                  PageContainer(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class PageContainer extends StatelessWidget {
+  const PageContainer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
